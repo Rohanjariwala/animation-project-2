f440e1ae22bb68d7497e8f96d206e9156391c3b3
Rohan Jariwala <rohanjariwala45@gmail.com>
2025-06-15 18:41:28 +0530

    updated animation and add loader

--- js/app.js
+++ js/app.js
@@ -7,17 +7,63 @@ document.addEventListener("DOMContentLoaded", (event) => {
   smoothTouch: 0.1,
 });
 
+/** loader animation **/
+let loader = gsap.timeline({
+  // onComplete: () => {
+  //   document.body.removeAttribute("style");
+  // }
+});
+loader.from('.stright_line',{
+  width:0,
+  duration:2,
+  delay:1,
+  ease:"circ.inOut"
+})
+loader.to('.loader .loading_inner span',{
+   backgroundImage:" linear-gradient(to right , #ffd687, #fc5243, #9d2f6a,#fff 100vh)",
+   duration:2,
+  ease:"circ.inOut"
+  },"-=1.99");
+  loader.to('.stright_line',{
+  opacity:0
+})
+loader.to('.loading_up',{
+  top:"-50%",
+  duration:0.8,
+  ease:"circ.inOut"
+})
+loader.to('.loading_down',{
+  bottom:"-50%",
+  duration:0.8,
+  ease:"circ.inOut"
+},"<");
+  loader.from('.animated_div',{
+    opacity:0,
+     scale:1.1,
+     duration:3,
+     ease: "power4.out", 
+     clearProps: "transform"
+    },"-=0.6");
+    loader.to('.loader',{
+      zIndex:-10,
+     },"-=0.6");
+     loader.to('body',{
+      overflow:'auto'
+     },"-=1.8");
+/** loader animation **/
 
-gsap.from('#hero',{opacity:0, scale:1.1,duration:3,ease: "power4.out"})
 
-  let tl = gsap.timeline({
+
+
+
+ let tl = gsap.timeline({
     scrollTrigger:{
         trigger:"#hero",
         start: 'top top', 
-		end: '+=3500',
+		    end: '+=3500',
         scrub: 1,
-        pin:true
-    }
+        pin:true,
+   }
   })
   tl.to('#hero .first_part .logo ',{
     opacity:0,
@@ -74,5 +120,15 @@ tl.to('#hero .second_part',{
     webkitMaskImage:"linear-gradient(to bottom, #000 -100%, #0000 20%)",
      duration:2
   },"-=2.30");
-  
+  tl.to('#hero h2',{
+     webkitMaskImage:"linear-gradient(to right, #0000 -200%, #000 100%)",
+     duration:2.5
+  },"-=1.4");
+  tl.to('#hero h2',{
+     backgroundImage:" linear-gradient(to right , #ffd687 -20vh, #fc5243, #9d2f6a)",
+     duration:2
+  },"-=1.8");
  });
+
+
+// tl.scrollTrigger.disable();
